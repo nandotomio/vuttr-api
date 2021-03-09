5860c69489f3800e8f0534cff25a1984581577a9
Fernando Tomio <nandotomio@gmail.com>
2021-03-09 16:17:04 -0300

    test: ensure SignUpController calls Validation with correct value

---
+++ tests/presentation/controllers/signup-controller.spec.ts
@@ -0,0 +1,37 @@
+import { SignUpController } from '@/presentation/controllers'
+import { ValidationSpy } from '@/tests/presentation/mocks'
+
+import faker from 'faker'
+
+const mockRequest = (): SignUpController.Request => {
+  const password = faker.internet.password()
+  return {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password,
+    passwordConfirmation: password
+  }
+}
+
+type SutTypes = {
+  sut: SignUpController
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const sut = new SignUpController(validationSpy)
+  return {
+    sut,
+    validationSpy
+  }
+}
+
+describe('SignUp Controller', () => {
+  test('Should call Validation with correct value', async () => {
+    const { sut, validationSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(validationSpy.input).toEqual(request)
+  })
+})
