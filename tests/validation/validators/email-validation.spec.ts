08700827c0f38b37c0c127334a95f899ec54cff6
Fernando Tomio <nandotomio@gmail.com>
2021-03-09 19:15:19 -0300

    test: ensure EmailValidation throws if EmailValidator throws

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -1,5 +1,6 @@
 import { EmailValidation } from '@/validation/validators'
 import { EmailValidatorSpy } from '@/tests/validation/mocks'
+import { throwError } from '@/tests/domain/mocks'
 
 import faker from 'faker'
 
@@ -25,5 +26,11 @@ describe('Email Validation', () => {
     const email = faker.internet.email()
     sut.validate({ [field]: email })
     expect(emailValidatorSpy.email).toBe(email)
+  })
+
+  test('Should throw if EmailValidator throws', () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    jest.spyOn(emailValidatorSpy, 'isValid').mockImplementationOnce(throwError)
+    expect(sut.validate).toThrow()
   })
 })
