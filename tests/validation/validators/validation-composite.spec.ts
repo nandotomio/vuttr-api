6ad30610a73676f831274180772b787fcd978e81
Fernando Tomio <nandotomio@gmail.com>
2021-03-09 19:30:17 -0300

    test: ensure ValidationComposite returns an error if any validation fails

---
+++ tests/validation/validators/validation-composite.spec.ts
@@ -0,0 +1,33 @@
+import { ValidationComposite } from '@/validation/validators'
+import { MissingParamError } from '@/presentation/errors'
+import { ValidationSpy } from '@/tests/presentation/mocks'
+
+import faker from 'faker'
+
+const field = faker.random.word()
+
+type SutTypes = {
+  sut: ValidationComposite
+  validationSpies: ValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpies = [
+    new ValidationSpy(),
+    new ValidationSpy()
+  ]
+  const sut = new ValidationComposite(validationSpies)
+  return {
+    sut,
+    validationSpies
+  }
+}
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationSpies } = makeSut()
+    validationSpies[1].error = new MissingParamError(field)
+    const error = sut.validate({ [field]: faker.random.word() })
+    expect(error).toEqual(validationSpies[1].error)
+  })
+})
