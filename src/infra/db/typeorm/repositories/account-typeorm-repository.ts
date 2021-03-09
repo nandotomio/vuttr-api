abbe71363e97d95d9ea60a548ab01372269d1b18
Fernando Tomio <nandotomio@gmail.com>
2021-03-09 13:54:02 -0300

    feat: ensure AccountMongoRepository checkByEmail throws EmailInUseError if email already in use

--- src/infra/db/typeorm/repositories/account-typeorm-repository.ts
+++ src/infra/db/typeorm/repositories/account-typeorm-repository.ts
@@ -1,14 +1,19 @@
-import { AddAccountRepository } from '@/data/protocols/db'
+import { AddAccountRepository, CheckAccountByEmailRepository } from '@/data/protocols/db'
 import { AccountTypeormEntity } from '@/infra/db'
+import { EmailInUseError } from '@/domain/errors'
 
 import { getMongoRepository } from 'typeorm'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, CheckAccountByEmailRepository {
   constructor (
     private readonly ormRepository = getMongoRepository(AccountTypeormEntity)
   ) {}
 
   async add (data: AddAccountRepository.Params): Promise<void> {
     await this.ormRepository.insertOne(data)
+  }
+
+  async checkByEmail (email: string): Promise<void> {
+    throw new EmailInUseError()
   }
 }
