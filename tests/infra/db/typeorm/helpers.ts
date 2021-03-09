343d2dad51ade3f1afd5ffcd6105521d762a8b2a
Fernando Tomio <nandotomio@gmail.com>
2021-03-09 16:06:00 -0300

    chore: update typeorm test config

--- tests/infra/db/typeorm/helpers.ts
+++ tests/infra/db/typeorm/helpers.ts
@@ -5,6 +5,7 @@ import { ConnectionOptions } from 'typeorm'
 export const ormConfig: ConnectionOptions = {
   type: 'mongodb',
   url: process.env.MONGO_URL,
+  useUnifiedTopology: true,
   useNewUrlParser: true,
   synchronize: true,
   logging: true,
