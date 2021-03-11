490fc1992396a8702ebe5e60d7d28a7ef84a7cec
Fernando Tomio <nandotomio@gmail.com>
2021-03-11 15:42:31 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- tests/main/decorators/log-controller-decorator.spec.ts
+++ tests/main/decorators/log-controller-decorator.spec.ts
@@ -1,6 +1,7 @@
 import { LogControllerDecorator } from '@/main/decorators'
 import { Controller, HttpResponse } from '@/presentation/protocols'
-import { ok } from '@/presentation/helpers'
+import { ok, serverError } from '@/presentation/helpers'
+import { LogErrorRepositorySpy } from '@/tests/data/mocks'
 
 import faker from 'faker'
 
@@ -14,17 +15,26 @@ class ControllerSpy implements Controller {
   }
 }
 
+const mockServerError = (): HttpResponse => {
+  const fakeError = new Error()
+  fakeError.stack = 'any_stack'
+  return serverError(fakeError)
+}
+
 type SutTypes = {
   sut: LogControllerDecorator
   controllerSpy: ControllerSpy
+  logErrorRepositorySpy: LogErrorRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const controllerSpy = new ControllerSpy()
-  const sut = new LogControllerDecorator(controllerSpy)
+  const logErrorRepositorySpy = new LogErrorRepositorySpy()
+  const sut = new LogControllerDecorator(controllerSpy, logErrorRepositorySpy)
   return {
     sut,
-    controllerSpy
+    controllerSpy,
+    logErrorRepositorySpy
   }
 }
 
@@ -40,5 +50,13 @@ describe('LogController Decorator', () => {
     const { sut, controllerSpy } = makeSut()
     const httpResponse = await sut.handle(faker.lorem.sentence())
     expect(httpResponse).toEqual(controllerSpy.httpResponse)
+  })
+
+  test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
+    const { sut, controllerSpy, logErrorRepositorySpy } = makeSut()
+    const serverError = mockServerError()
+    controllerSpy.httpResponse = serverError
+    await sut.handle(faker.lorem.sentence())
+    expect(logErrorRepositorySpy.stack).toBe(serverError.body.stack)
   })
 })
