08b55b1b4f2f538bc17d49803bdece769241db96
Fernando Tomio <nandotomio@gmail.com>
2021-03-11 15:35:38 -0300

    test: ensure LogControllerDecorator calls controller with correct value

---
+++ tests/main/decorators/log-controller-decorator.spec.ts
@@ -0,0 +1,38 @@
+import { LogControllerDecorator } from '@/main/decorators'
+import { Controller, HttpResponse } from '@/presentation/protocols'
+import { ok } from '@/presentation/helpers'
+
+import faker from 'faker'
+
+class ControllerSpy implements Controller {
+  httpResponse = ok(faker.random.uuid())
+  request: any
+
+  async handle (request: any): Promise<HttpResponse> {
+    this.request = request
+    return this.httpResponse
+  }
+}
+
+type SutTypes = {
+  sut: LogControllerDecorator
+  controllerSpy: ControllerSpy
+}
+
+const makeSut = (): SutTypes => {
+  const controllerSpy = new ControllerSpy()
+  const sut = new LogControllerDecorator(controllerSpy)
+  return {
+    sut,
+    controllerSpy
+  }
+}
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    const { sut, controllerSpy } = makeSut()
+    const request = faker.lorem.sentence()
+    await sut.handle(request)
+    expect(controllerSpy.request).toEqual(request)
+  })
+})
