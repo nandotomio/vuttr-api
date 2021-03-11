bf7ece31b9ac6be74ca0dc9db2616e5a0f0a673a
Fernando Tomio <nandotomio@gmail.com>
2021-03-11 20:59:08 -0300

    feat: ensure login route returns 200 on login

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -1,8 +1,9 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController } from '@/main/factories'
+import { makeLoginController, makeSignUpController } from '@/main/factories'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
