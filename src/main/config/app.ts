e6b6a476e3f3702fb757e20a2943065f4994894e
Fernando Tomio <nandotomio@gmail.com>
2021-03-13 18:22:45 -0300

    feat: add api docs route

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,3 +1,4 @@
+import setupSwagger from './swagger'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 
@@ -5,6 +6,7 @@ import express, { Application } from 'express'
 
 export default (): Application => {
   const app = express()
+  setupSwagger(app)
   setupMiddlewares(app)
   setupRoutes(app)
   return app
