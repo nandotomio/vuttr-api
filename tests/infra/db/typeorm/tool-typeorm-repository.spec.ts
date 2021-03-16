7aeae5eeac8c3e3c3c617e8d9ccf69bf353341b3
Fernando Tomio <nandotomio@gmail.com>
2021-03-16 13:36:32 -0300

    test: ensure ToolTypeormRepository returns a tool on success

--- tests/infra/db/typeorm/tool-typeorm-repository.spec.ts
+++ tests/infra/db/typeorm/tool-typeorm-repository.spec.ts
@@ -32,5 +32,16 @@ describe('ToolTypeormRepository', () => {
       const count = await toolRepository.count()
       expect(count).toBe(1)
     })
+
+    test('Should return a tool on success', async () => {
+      const sut = makeSut()
+      const addToolParams = mockAddToolParams()
+      const tool = await sut.add(addToolParams)
+      expect(tool.id).toBeTruthy()
+      expect(tool.title).toBe(addToolParams.title)
+      expect(tool.link).toBe(addToolParams.link)
+      expect(tool.description).toBe(addToolParams.description)
+      expect(tool.tags).toEqual(addToolParams.tags)
+    })
   })
 })
